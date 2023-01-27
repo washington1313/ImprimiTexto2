ab2fa98c5ffcb3555c1e5621ddce3492da08a852
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-27 18:16:01 -0300

    Projeto Finalizado

---
+++ ImprimirTexto2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImprimirTexto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFInal;
+
+            Console.WriteLine("Digite seu nome: ");
+            string nomeEntrada = Console.ReadLine();
+            Console.Clear();
+
+            nomeFInal = menuFinal(nomeEntrada);
+            Console.WriteLine(nomeFInal);
+
+            Console.ReadKey();
+        }
+
+        static string menuFinal (string nome)
+        {
+            string texto = "########## MENU ############\n"+
+                                       nome+ "\n"
++                            "############################";
+
+            return texto;
+        }
+
+    }
+}
